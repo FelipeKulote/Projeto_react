65d7bfc8bbf3a363dcee6e629651a5bea11468bb
Felipe Augusto <lipe_augustobq@hotmail.com>
2022-10-26 21:20:26 -0300

    feat: action mode na criação do card

--- cards_react/src/components/form/form.jsx
+++ cards_react/src/components/form/form.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../utils/api/api";
 import "./form.css";
 
@@ -14,62 +14,68 @@ export function Form({ getAll }) {
     setLoading(false);
   }
 
+  useEffect(() => {
+    getAll();
+  }, [loading]);
+
   return (
     <>
-    { loading ? (<div className="loading"> Carregando novo card... </div>) : (
-      <section className="form">
-        <form onSubmit={handleSubmit} className="form-input">
-          <div className="titulo-form">Adicionar nova carta</div>
-          <div>
-            <span>Nome:</span>
-            <input
-              type="text"
-              name="name"
-              required
-              onChange={(e) => {
-                setNewCard({ ...newCard, name: e.target.value });
-              }}
-            />
-          </div>
-          <div>
-            <span>Tipo:</span>
-            <input
-              type="text"
-              name="type"
-              required
-              onChange={(e) => {
-                setNewCard({ ...newCard, type: e.target.value });
-              }}
-            />
-          </div>
-          <div>
-            <span>ATK:</span>
-            <input
-              type="number"
-              name="atk"
-              required
-              onChange={(e) => {
-                setNewCard({ ...newCard, atk: e.target.value });
-              }}
-            />
-          </div>
-          <div>
-            <span>DEF:</span>
-            <input
-              type="number"
-              name="def"
-              required
-              onChange={(e) => {
-                setNewCard({ ...newCard, def: e.target.value });
-              }}
-            />
-          </div>
-          <button type="submit" className="submit_btn">
-            Enviar
-          </button>
-        </form>
-      </section>
-)}
+      {loading ? (
+        <div className="loading"> Carregando novo card... </div>
+      ) : (
+        <section className="form">
+          <form onSubmit={handleSubmit} className="form-input">
+            <div className="titulo-form">Adicionar nova carta</div>
+            <div>
+              <span>Nome:</span>
+              <input
+                type="text"
+                name="name"
+                required
+                onChange={(e) => {
+                  setNewCard({ ...newCard, name: e.target.value });
+                }}
+              />
+            </div>
+            <div>
+              <span>Tipo:</span>
+              <input
+                type="text"
+                name="type"
+                required
+                onChange={(e) => {
+                  setNewCard({ ...newCard, type: e.target.value });
+                }}
+              />
+            </div>
+            <div>
+              <span>ATK:</span>
+              <input
+                type="number"
+                name="atk"
+                required
+                onChange={(e) => {
+                  setNewCard({ ...newCard, atk: e.target.value });
+                }}
+              />
+            </div>
+            <div>
+              <span>DEF:</span>
+              <input
+                type="number"
+                name="def"
+                required
+                onChange={(e) => {
+                  setNewCard({ ...newCard, def: e.target.value });
+                }}
+              />
+            </div>
+            <button type="submit" className="submit_btn">
+              Enviar
+            </button>
+          </form>
+        </section>
+      )}
     </>
   );
 }
